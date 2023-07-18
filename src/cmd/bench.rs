685c09b91a14d36923a859e4a150731a533dd7ba
joecryptotoo <80373433+joecryptotoo@users.noreply.github.com>
2023-07-18 09:44:00 -0700

    Report duration of each iteration

--- src/cmd/bench.rs
+++ src/cmd/bench.rs
@@ -18,13 +18,14 @@ pub struct Cmd {
 impl Cmd {
     pub fn run(&self, device: &Device) -> Result {
         let keypair = device.get_keypair(false)?;
-        let duration = bench_sign(&keypair, self.iterations)?;
+        let (duration, durations) = bench_sign(&keypair, self.iterations)?;
         let rate = self.iterations as f64 / duration.as_secs_f64();
         let avg_ms = duration.as_millis() as f64 / self.iterations as f64;
         let json = json!({
             "iterations": self.iterations,
             "avg_ms": round2(avg_ms),
             "rate": round2(rate),
+            "durations": durations.into_iter().map(|d| d.as_millis()).collect::<Vec<_>>(),
         });
         print_json(&json)
     }
@@ -34,15 +35,18 @@ fn round2(v: f64) -> f64 {
     (v * 100.0).round() / 100.0
 }
 
-fn bench_sign(keypair: &Keypair, iterations: u32) -> Result<Duration> {
+fn bench_sign(keypair: &Keypair, iterations: u32) -> Result<(Duration, Vec<Duration>)> {
     let mut total_duration = Duration::new(0, 0);
+    let mut durations = Vec::new();
     for _ in 1..iterations {
         let mut data = [0u8; 32];
         OsRng.try_fill_bytes(&mut data)?;
 
         let start = Instant::now();
         let _signature = keypair.sign(&data)?;
-        total_duration += start.elapsed();
+        let duration = start.elapsed();
+        total_duration += duration;
+        durations.push(duration);
     }
-    Ok(total_duration)
+    Ok((total_duration, durations))
 }
